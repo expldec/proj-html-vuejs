7767492132cbca5b7dbe110d3e48048bb4901e75
expldec <explosive.deconstruction@gmail.com>
2022-05-30 17:27:49 +0200

    added and styled template promo section

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -16,7 +16,59 @@
         </div>
       </div>
     </section>
-    <section class="template-promo"></section>
+    <!-- TEMPLATE PROMO -->
+    <section class="template-promo">
+      <div class="container">
+        <div class="row justify-content-center">
+          <div class="col-6 text-center">
+            <h3>Meet the new agency SEO Template From the Avada Team</h3>
+            <p class="text-muted pb-5">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              Voluptatem atque aliquam est eos expedita beatae.
+            </p>
+          </div>
+        </div>
+        <div class="row gx-3 text-center">
+          <div class="col-4">
+            <div class="bg-white p-5">
+              <i class="fas fa-road"></i>
+              <h4>Establish Goals</h4>
+              <p class="text-muted">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
+                eius, dicta voluptatum dolore sed pariatur! Expedita facilis
+                assumenda natus fugit!
+              </p>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="bg-white p-5">
+              <i class="fas fa-video"></i>
+              <h4>Work with a Team</h4>
+              <p class="text-muted">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
+                eius, dicta voluptatum dolore sed pariatur! Expedita facilis
+                assumenda natus fugit!
+              </p>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="bg-white p-5">
+              <i class="fas fa-tint"></i>
+              <h4>Get Results</h4>
+              <p class="text-muted">
+                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Modi
+                eius, dicta voluptatum dolore sed pariatur! Expedita facilis
+                assumenda natus fugit!
+              </p>
+            </div>
+          </div>
+        </div>
+        <img
+          src="../assets/images/agency-seo-desk-front.jpg"
+          alt="A nice desk"
+        />
+      </div>
+    </section>
     <section class="services"></section>
     <section class="our-work"></section>
     <section class="pricing"></section>
@@ -33,14 +85,36 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import "../style/variables.scss";
+@import "../style/common.scss";
+img {
+  max-width: 100%;
+}
+h2 {
+  font-size: 3rem;
+  font-weight: 700;
+}
+h3 {
+  font-size: 2rem;
+  font-weight: 700;
+}
+h4 {
+  font-size: 1.5rem;
+  font-weight: 700;
+  margin: 1rem 0;
+}
 .jumbo {
   background-image: url("../assets/images/1-hero-image.png");
   background-size: cover;
   background-position: center;
   height: 100vh;
 }
-h2 {
-  font-size: 3rem;
-  font-weight: 700;
+.template-promo {
+  background-color: $background-light;
+  padding-top: 4rem;
+  i {
+    font-size: 3rem;
+    color: $primary;
+  }
 }
 </style>
